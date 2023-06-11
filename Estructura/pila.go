06cde00a9e27454f93bff06693c27712e8334391
Daniel García <88566114+DanielGarcia-201901103@users.noreply.github.com>
2023-06-11 14:24:07 -0600

    agregando pila, faltan agregar mas funciones al inicio de sesion empleado

--- Estructura/pila.go
+++ Estructura/pila.go
@@ -1,5 +1,9 @@
 package estructura
 
+import (
+	"fmt"
+)
+
 type Pedido struct {
 	idCliente  string
 	idEmpleado string
@@ -23,5 +27,38 @@ func (pila *Pila) Push(idCliente string, idEmpleado string, nameImagen string, i
 		nuevoN := &NodoPila{data: nuevoPedido, siguiente: nil}
 		pila.Primero = nuevoN
 		pila.Longitud++
+	} else {
+		nuevoN := &NodoPila{data: nuevoPedido, siguiente: pila.Primero}
+		pila.Primero = nuevoN
+		pila.Longitud++
 	}
+}
+
+func (pila *Pila) Pop() {
+	if pila.Longitud == 0 {
+		fmt.Println("No hay elementos en la pila")
+	} else {
+		pila.Primero = pila.Primero.siguiente
+		pila.Longitud--
+	}
+}
+
+func (pila *Pila) ReportePila() {
+	nombreArchivo := "./pila.dot"
+	nombreImagen := "./pila.jpg"
+	text := "digraph pila{\n"
+	text += "rankdir = LR; \n"
+	text += "node[shape = record]; \n"
+	aux := pila.Primero
+	text += "nodo0 [label=\""
+	//text += "nodonull1->nodo0 [dir=back];\n"
+	for i := 0; i < pila.Longitud; i++ {
+		text += "|(ID Cliente: " + aux.data.idCliente + "\\" + "n Imagen:" + aux.data.nameImagen +
+			")"
+		aux = aux.siguiente
+	}
+	text += "\"]; \n}"
+	crearArchivo(nombreArchivo)
+	escribirArchivo(text, nombreArchivo)
+	ejecutar(nombreImagen, nombreArchivo)
 }
